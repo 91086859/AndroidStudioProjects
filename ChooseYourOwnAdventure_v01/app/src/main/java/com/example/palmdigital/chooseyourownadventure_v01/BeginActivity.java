2f4ee7f7a3d061a65d06f48774dea457e00c78a6
91086859 <32179176+91086859@users.noreply.github.com>
2018-04-09 10:17:49 -0700

    CYOA commit

--- ChooseYourOwnAdventure_v01/app/src/main/java/com/example/palmdigital/chooseyourownadventure_v01/BeginActivity.java
+++ ChooseYourOwnAdventure_v01/app/src/main/java/com/example/palmdigital/chooseyourownadventure_v01/BeginActivity.java
@@ -2,49 +2,44 @@ package com.example.palmdigital.chooseyourownadventure_v01;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class BeginActivity extends AppCompatActivity implements View.OnClickListener {
+    Button wakeup;
+    Button explore;
 
-    Button startButton;
-    MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_begin);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        startButton = findViewById(R.id.startbutton);
-        startButton.setOnClickListener(this);
+        wakeup = findViewById(R.id.button_wake);
+        wakeup.setOnClickListener(this);
 
-        GifImageView gifImageView = findViewById(R.id.GifImageView);
-        gifImageView.setGifImageResource(R.drawable.sleeping);
-
-        mp = MediaPlayer.create(this, R.raw.dreamharp);
+        explore = findViewById(R.id.button_explore);
+        explore.setOnClickListener(this);
     }
 
     public void onClick(View view)
     {
-        if(view.getId() == R.id.startbutton)
+        if(view.getId() == R.id.button_wake)
         {
-            Intent i = new Intent(this, BeginActivity.class);
+            Intent i = new Intent(this, WakeUpActivity.class);
             startActivity(i);
             overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
             finish();
-            mp.start();
         }
         else if(view.getId() == R.id.button_explore)
         {
             Intent i2 = new Intent(this, ExploreActivity.class);
             startActivity(i2);
-
+            overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+            finish();
         }
     }
 }
--- ChooseYourOwnAdventure_v01/app/src/main/java/com/example/palmdigital/chooseyourownadventure_v01/FaintActivity.java
+++ ChooseYourOwnAdventure_v01/app/src/main/java/com/example/palmdigital/chooseyourownadventure_v01/FaintActivity.java
@@ -2,10 +2,16 @@ package com.example.palmdigital.chooseyourownadventure_v01;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class FaintActivity extends AppCompatActivity implements View.OnClickListener {
 
